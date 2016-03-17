76639c7c38bf93d7b7d451c97d27e06389cdddc9
totalintelli <blueintelli@gmail.com>
2016-03-17 16:56:32 +0900

    가위바위보 게임 구현

---
+++ RockpaperscissorsGameWindowsForms/EventForm.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace RockpaperscissorsGameWindowsForms
+{
+    partial class EventForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lb_result = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_paper = new System.Windows.Forms.Button();
+            this.btn_rock = new System.Windows.Forms.Button();
+            this.btn_scissors = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.lb_result);
+            this.groupBox1.Controls.Add(this.pictureBox2);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.btn_paper);
+            this.groupBox1.Controls.Add(this.btn_rock);
+            this.groupBox1.Controls.Add(this.btn_scissors);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(259, 221);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "가위바위보 게임";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(181, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "COM";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 139);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Player";
+            // 
+            // lb_result
+            // 
+            this.lb_result.AutoSize = true;
+            this.lb_result.Location = new System.Drawing.Point(71, 167);
+            this.lb_result.Name = "lb_result";
+            this.lb_result.Size = new System.Drawing.Size(75, 12);
+            this.lb_result.TabIndex = 5;
+            this.lb_result.Text = "승부 결과는?";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(148, 50);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 78);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(6, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 82);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btn_paper
+            // 
+            this.btn_paper.Location = new System.Drawing.Point(181, 21);
+            this.btn_paper.Name = "btn_paper";
+            this.btn_paper.Size = new System.Drawing.Size(75, 23);
+            this.btn_paper.TabIndex = 2;
+            this.btn_paper.Text = "보";
+            this.btn_paper.UseVisualStyleBackColor = true;
+            this.btn_paper.Click += new System.EventHandler(this.btn_paper_clicked);
+            // 
+            // btn_rock
+            // 
+            this.btn_rock.Location = new System.Drawing.Point(100, 20);
+            this.btn_rock.Name = "btn_rock";
+            this.btn_rock.Size = new System.Drawing.Size(75, 23);
+            this.btn_rock.TabIndex = 1;
+            this.btn_rock.Text = "바위";
+            this.btn_rock.UseVisualStyleBackColor = true;
+            this.btn_rock.Click += new System.EventHandler(this.btn_rock_clicked);
+            // 
+            // btn_scissors
+            // 
+            this.btn_scissors.Location = new System.Drawing.Point(19, 21);
+            this.btn_scissors.Name = "btn_scissors";
+            this.btn_scissors.Size = new System.Drawing.Size(75, 23);
+            this.btn_scissors.TabIndex = 0;
+            this.btn_scissors.Text = "가위";
+            this.btn_scissors.UseVisualStyleBackColor = true;
+            this.btn_scissors.Click += new System.EventHandler(this.btn_scissors_clicked);
+            // 
+            // EventForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "EventForm";
+            this.Text = "가위바위보 게임기";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btn_scissors;
+        private System.Windows.Forms.Button btn_paper;
+        private System.Windows.Forms.Button btn_rock;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lb_result;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ RockpaperscissorsGameWindowsForms/EventForm.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RockpaperscissorsGameWindowsForms
+{
+
+
+    public partial class EventForm : Form
+    {
+        public delegate void EventHandler(Results Result, Status ExchangedComputerChoice);
+        public event EventHandler PopUpEvent;
+        
+        int UserChoice; // 사용자의 선택값
+
+        public enum Status
+        {
+            Scissors = 0,
+            Rock = 1,
+            Paper = 2,
+            None = -1
+        }
+
+        public enum Results
+        {
+            Win = 0,
+            Draw = 1,
+            Lose = 2,
+            None = -1
+        }
+
+
+        public EventForm()
+        {
+            InitializeComponent();
+
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void DoSomething()
+        {
+            Results Result; // 게임 결과
+            Status ExchangedComputerChoice;
+            
+            EventForm Form = new EventForm();
+
+            Play(UserChoice, out Result, out ExchangedComputerChoice);
+
+            PopUpEvent(Result, ExchangedComputerChoice);
+        }
+
+        private void btn_scissors_clicked(object sender, EventArgs e)
+        {
+            EventForm Form = new EventForm();
+
+            Form.UserChoice = 0;
+
+            pictureBox1.Load(@"E:\Projects\RockpaperscissorsGameWindowsForms\RockpaperscissorsGameWindowsForms\Scissors.png");
+
+            Form.PopUpEvent += new EventHandler(PopupMessage);
+            Form.DoSomething();
+        }
+
+
+        private void btn_rock_clicked(object sender, EventArgs e)
+        {
+            EventForm Form = new EventForm();
+
+            Form.UserChoice = 1;
+
+            pictureBox1.Load(@"E:\Projects\RockpaperscissorsGameWindowsForms\RockpaperscissorsGameWindowsForms\Rock.png");
+
+            Form.PopUpEvent += new EventHandler(PopupMessage);
+            Form.DoSomething();
+        }
+
+        private void btn_paper_clicked(object sender, EventArgs e)
+        {
+            EventForm Form = new EventForm();
+
+            Form.UserChoice = 2;
+
+            pictureBox1.Load(@"E:\Projects\RockpaperscissorsGameWindowsForms\RockpaperscissorsGameWindowsForms\Paper.png");
+
+            Form.PopUpEvent += new EventHandler(PopupMessage);
+            Form.DoSomething();
+        }
+
+        private void Play(int UserChoice, out Results Results, out Status ExchangedComputerChoice)
+        {
+            Results = Results.None;                   // 게임 결과
+            int ComputerChoice;                               // 컴퓨터의 선택값
+            Status ExchangedUserChoice = Status.None;     // 사용자 선택의 변환값
+            ExchangedComputerChoice = Status.None;  // 컴퓨터의 선택값의 변환값
+
+            // 사용자 선택의 변환값을 구한다.
+            switch (UserChoice)
+            {
+                case 0:
+                    ExchangedUserChoice = Status.Scissors;
+                    break;
+                case 1:
+                    ExchangedUserChoice = Status.Rock;
+                    break;
+                case 2:
+                    ExchangedUserChoice = Status.Paper;
+                    break;
+                default:
+                    break;
+            }
+
+            // 컴퓨터의 선택을 구한다.
+            Random Random = new Random();
+            ComputerChoice = Random.Next(0, 3);
+
+            // 컴퓨터 선택의 변환값을 구한다.
+            switch (ComputerChoice)
+            {
+                case 0:
+                    ExchangedComputerChoice = Status.Scissors;
+                    break;
+                case 1:
+                    ExchangedComputerChoice = Status.Rock;
+                    break;
+                case 2:
+                    ExchangedComputerChoice = Status.Paper;
+                    break;
+                default:
+                    break;
+            }
+
+            // 가위바위보 게임을 한다.
+            if (ExchangedUserChoice == Status.Scissors)
+            {
+                switch (ExchangedComputerChoice)
+                {
+                    case Status.Scissors:
+                        Results = Results.Draw;
+                        break;
+                    case Status.Rock:
+                        Results = Results.Lose;
+                        break;
+                    case Status.Paper:
+                        Results = Results.Win;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            if (ExchangedUserChoice == Status.Rock)
+            {
+                switch (ExchangedComputerChoice)
+                {
+                    case Status.Scissors:
+                        Results = Results.Win;
+                        break;
+                    case Status.Rock:
+                        Results = Results.Draw;
+                        break;
+                    case Status.Paper:
+                        Results = Results.Lose;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            if (ExchangedUserChoice == Status.Paper)
+            {
+                switch (ExchangedComputerChoice)
+                {
+                    case Status.Scissors:
+                        Results = Results.Lose;
+                        break;
+                    case Status.Rock:
+                        Results = Results.Win;
+                        break;
+                    case Status.Paper:
+                        Results = Results.Draw;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        public void PopupMessage(Results Result, Status ExchangedComputerChoice)
+        {
+            EventForm Form = new EventForm();
+
+
+            // 컴퓨터 선택의 변환값을 화면에 표시한다.
+            switch (ExchangedComputerChoice)
+            {
+                case Status.Scissors:
+                    pictureBox2.Load(@"E:\Projects\RockpaperscissorsGameWindowsForms\RockpaperscissorsGameWindowsForms\Scissors.png");
+                    break;
+                case Status.Rock:
+                    pictureBox2.Load(@"E:\Projects\RockpaperscissorsGameWindowsForms\RockpaperscissorsGameWindowsForms\Rock.png");
+                    break;
+                case Status.Paper:
+                    pictureBox2.Load(@"E:\Projects\RockpaperscissorsGameWindowsForms\RockpaperscissorsGameWindowsForms\Paper.png");
+                    break;
+                default:
+                    break;
+            }
+
+            switch (Result)
+            {
+                case Results.Win:
+                    lb_result.Text = "당신은 이겼습니다.";
+                    break;
+                case Results.Draw:
+                    lb_result.Text = "당신은 비겼습니다.";
+                    break;
+                case Results.Lose:
+                    lb_result.Text = "당신은 졌습니다.";
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
